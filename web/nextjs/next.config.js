e4368e8c0f5cf2b7f010435cb602eb78531bb436
Robert Sturla <robertsturla@outlook.com>
2022-02-07 21:11:46 +0000

    first commit

---
+++ web/nextjs/next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const { DEPRECATED_URL } = "http://localhost:4200/deprecated/"
+
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
+
+module.exports = {
+  async rewrites() {
+    return [
+      // Check if the content exists in Next.js
+      {
+        source: '/:path*',
+        destination: '/:path*',
+      }
+    ]
+  }
+}
